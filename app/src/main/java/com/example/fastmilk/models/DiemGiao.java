4355271d7533154473066daf8096c1ac5e007ee2
Nhan <nhannt1938@gmail.com>
2021-10-30 19:12:33 +0700

    add toa do vao models diem giao

--- app/src/main/java/com/example/fastmilk/models/DiemGiao.java
+++ app/src/main/java/com/example/fastmilk/models/DiemGiao.java
@@ -4,14 +4,18 @@ public class DiemGiao {
     private int idDiemGiao;
     private String tenDiemGiao;
     private int idKhuVuc;
+    private float longitude;
+    private float latitude;
     private String diaChi;
     private String tenKhuVuc;
     private String trangThai;
 
-    public DiemGiao(int idDiemGiao, String tenDiemGiao, int idKhuVuc, String diaChi, String tenKhuVuc, String trangThai) {
+    public DiemGiao(int idDiemGiao, String tenDiemGiao, int idKhuVuc, float longitude, float latitude, String diaChi, String tenKhuVuc, String trangThai) {
         this.idDiemGiao = idDiemGiao;
         this.tenDiemGiao = tenDiemGiao;
         this.idKhuVuc = idKhuVuc;
+        this.longitude = longitude;
+        this.latitude = latitude;
         this.diaChi = diaChi;
         this.tenKhuVuc = tenKhuVuc;
         this.trangThai = trangThai;
@@ -42,6 +46,22 @@ public class DiemGiao {
 
     public void setIdKhuVuc(int idKhuVuc) {
         this.idKhuVuc = idKhuVuc;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
     }
 
     public String getDiaChi() {
