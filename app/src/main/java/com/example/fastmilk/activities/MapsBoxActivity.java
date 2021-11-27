b7e8d0070a6511e2bdaf2c93240c86c070f82ce1
vuanhtuan2912a <86229187+vuanhtuan2912a@users.noreply.github.com>
2021-11-27 20:28:38 +0700

    xong User Location

--- app/src/main/java/com/example/fastmilk/activities/MapsBoxActivity.java
+++ app/src/main/java/com/example/fastmilk/activities/MapsBoxActivity.java
@@ -1,20 +1,48 @@
 package com.example.fastmilk.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.fastmilk.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdate;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.maps.Style.OnStyleLoaded;
 
-public class MapsBoxActivity extends AppCompatActivity {
+import java.util.List;
+
+import static com.mapbox.mapboxsdk.maps.Style.LIGHT;
+
+public class MapsBoxActivity extends AppCompatActivity implements PermissionsListener, OnMapReadyCallback {
+
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private LocationComponent locationComponent;
+    private PermissionsManager permissionsManager;
+    private FloatingActionButton centerLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +50,71 @@ public class MapsBoxActivity extends AppCompatActivity {
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_maps_box);
 
+        centerLocation = (FloatingActionButton) findViewById(R.id.centerLocation);
+
         // Initiation of the MapView
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mapView.getMapAsync(this);
+
+
+        centerLocation.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        Toast.makeText(MapsBoxActivity.this, "Maps has been loaded", Toast.LENGTH_SHORT).show();
-                    }
-                });
+            public void onClick(View v) {
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(locationComponent.getLastKnownLocation().getLatitude() , locationComponent.getLastKnownLocation().getLongitude()))
+                        .zoom(12)
+                        .build();
+                        mapboxMap.setCameraPosition(cameraPosition);
+                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 500);
             }
         });
+    }
 
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/tuantutu2912a/ckwhsylkd1lrz14mmq09m9z23"),
+                style -> enableComponent(style));
+    }
+
+    public void enableComponent(@NonNull Style loadedMapStyle) {
+        try {
+            // check if permissions are enabled and if not reqest
+            if (PermissionsManager.areLocationPermissionsGranted(this)) {
+                // get an instance of the component
+                locationComponent = mapboxMap.getLocationComponent();
+
+                // Activate with options
+               locationComponent.activateLocationComponent(
+                       LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+                // Enable to make component visible
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationComponent.setLocationComponentEnabled(true);
+
+                // Set the component's camera mode
+                locationComponent.setCameraMode(CameraMode.TRACKING);
+
+                // Set the component's render mode
+                locationComponent.setRenderMode(RenderMode.COMPASS);
+
+            } else {
+                permissionsManager = new PermissionsManager(this);
+                permissionsManager.requestLocationPermissions(this);
+
+            }
+        } catch (Exception e) {
+            Log.e("ERR_Load_MAP", e.getMessage());
+        }
     }
 
     @Override
@@ -63,6 +141,8 @@ public class MapsBoxActivity extends AppCompatActivity {
         mapView.onStop();
     }
 
+
+
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -79,5 +159,34 @@ public class MapsBoxActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
